c053f5ef9e2b883678a55a06001c274b2250e446
Rajeswari badiger <149804235+rajeswaribadiger2004@users.noreply.github.com>
2025-08-03 19:28:37 +0300

    Update 4_Factorial.c

--- 4_Factorial.c
+++ 4_Factorial.c
@@ -13,3 +13,32 @@ fact=fact*i;
 printf("Factorial of %d is: %d\n",number,fact);
 return 0;
 }
+
+
+//RECURSION
+#include<stdio.h>
+
+// Recursive function to calculate factorial
+int factorial(int n) {
+    if(n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int number;
+    printf("Enter a number: \n");
+    scanf("%d", &number);
+
+    if (number < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        int fact = factorial(number);
+        printf("Factorial of %d is: %d\n", number, fact);
+    }
+
+    return 0;
+}
+
+
