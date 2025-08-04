6abb86acd7a476641e2cedfb56713041dd7b5400
Rajeswari badiger <149804235+rajeswaribadiger2004@users.noreply.github.com>
2025-08-04 19:56:22 +0300

    Create 16_leap.c

---
+++ 16_leap.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() 
+{
+    int y;
+    printf("Enter year: ");
+    scanf("%d", &y);
+
+    if (y % 4 == 0) {
+        if (y % 100 == 0) {
+            if (y % 400 == 0)
+                printf("%d is a Leap Year\n", y);
+            else
+                printf("%d is not a Leap Year\n", y);
+        } else {
+            printf("%d is a Leap Year\n", y);
+        }
+    } else {
+        printf("%d is not a Leap Year\n", y);
+    }
+
+    return 0;
+}
