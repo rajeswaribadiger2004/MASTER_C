3e9025b1d708dd50b59c2d320e3823ae547357c4
Rajeswari badiger <149804235+rajeswaribadiger2004@users.noreply.github.com>
2025-08-07 19:50:12 +0300

    Update 40_odd_even.c

--- 40_odd_even.c
+++ 40_odd_even.c
@@ -4,7 +4,7 @@ int main()
  int num;
  printf("Enter any number: ");
  scanf("%d", &num);
- if(num & 1)
+ if(num & 1)                                      //odd &1 ==1   //even & 1==0
  {
  printf("%d is odd.", num);
  }
