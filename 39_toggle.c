1937baf0981884b02debbb645556833e0b2b7d6c
Rajeswari badiger <149804235+rajeswaribadiger2004@users.noreply.github.com>
2025-08-07 19:41:51 +0300

    Create 39_toggle.c

---
+++ 39_toggle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+ int num, n, newNum;
+ printf("Enter any number: ");
+ scanf("%d", &num);
+ printf("Enter nth bit to toggle (0-31): ");
+ scanf("%d", &n);
+ newNum = num ^ (1 << n);
+ printf("Number before toggling %d bit: %d (in decimal)\n", n, num);
+ printf("Number after toggling %d bit: %d (in decimal)\n", n, newNum);
+ return 0;
+}
