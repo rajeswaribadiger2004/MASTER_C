91e9bafdd4d79c791ce9a401bf79e757fdd97edc
Rajeswari badiger <149804235+rajeswaribadiger2004@users.noreply.github.com>
2025-08-07 19:35:57 +0300

    Create 38_nth_bit.c

---
+++ 38_nth_bit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+ int num, n, newNum;
+ printf("Enter any number: ");
+ scanf("%d", &num);
+ printf("Enter nth bit to set (0-31): ");
+ scanf("%d", &n);
+ newNum = (1 << n) | num;                           //bitstatus=num>>n&1;
+ printf("Bit set successfully.\n\n");
+ printf("Number before setting %d bit: %d (in decimal)\n", n, num);
+ printf("Number after setting %d bit: %d (in decimal)\n", n, newNum);
+ return 0;
+}
